0b64e55b536465f081a777022e6da4dbcc6def45
Yoni Friedman <yyf@mit.edu>
2024-07-10 22:57:36 -0400

    modify slurm script for scaled view

--- slurm_launch.sh
+++ slurm_launch.sh
@@ -14,4 +14,14 @@
 
 IDX=$SLURM_ARRAY_TASK_ID
 START_SCENE=$((IDX * 20))
-/scratch2/weka/tenenbaum/yyf/blender-3.2.1-linux-x64/blender -b -noaudio -P generate_scene.py -- --start_scene $START_SCENE --n_scenes 20 --data_dir /om2/user/yyf/textured-shapes/texture-sweep/ --resolution 512 --shape_type shapegen --max_trajectories 1 --num_trajectories 1 --textures_per_scene 28 --no_overwrite
+#/scratch2/weka/tenenbaum/yyf/blender-3.2.1-linux-x64/blender -b -noaudio -P generate_scene.py -- --start_scene $START_SCENE --n_scenes 20 --data_dir /om2/user/yyf/textured-shapes/texture-sweep/ --resolution 512 --shape_type shapegen --max_trajectories 1 --num_trajectories 1 --textures_per_scene 28 --no_overwrite
+
+if [$IDX -eq 0]; then
+
+	# small zoom
+	/scratch2/weka/tenenbaum/yyf/blender-3.2.1-linux-x64/blender -b -noaudio -P generate_scene.py -- --start_scene 0 --n_scenes 20 --data_dir /om2/user/yyf/textured-shapes/scaled-hi-res/small/ --resolution 1024 --shape_type shapegen --max_trajectories 1 --num_trajectories 1 --textures_per_scene 7 --alternate_obj_directory /om2/user/yyf/textured-shapes/hi-res-scenes/ --focal_length small
+
+else
+	# medium zoom
+	/scratch2/weka/tenenbaum/yyf/blender-3.2.1-linux-x64/blender -b -noaudio -P generate_scene.py -- --start_scene 0 --n_scenes 20 --data_dir /om2/user/yyf/textured-shapes/scaled-hi-res/medium/ --resolution 1024 --shape_type shapegen --max_trajectories 1 --num_trajectories 1 --textures_per_scene 7 --alternate_obj_directory /om2/user/yyf/textured-shapes/hi-res-scenes/ --focal_length medium
+fi
